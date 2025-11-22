226554c0cc36554b81830044d41ebaebc0075b0c
DevarshiKothari <devarshikothari1998@gmail.com>
2025-11-22 17:51:38 +0530

    # Count K-Length Substrings With No Repeated Characters

--- SlidingWindow4.py
+++ SlidingWindow4.py
@@ -4,13 +4,13 @@
 class SlidingWindow:
     def operation(s: str, k: int) -> int:
         uniqueChar = set()
-        start, ans = 0, 0
+        i, start, ans = 0, 0, 0
         l = len(s)
         if l < k:
             return 0
 
-        for i in range(l - k + 1):
-            while len(uniqueChar) < k:
+        while i < l - k + 1:
+            while len(uniqueChar) < k and i < l - k + 1:
                 if s[start] not in uniqueChar:
                     uniqueChar.add(s[start])
                     start += 1
@@ -20,13 +20,15 @@ class SlidingWindow:
                         i += 1
                     uniqueChar.add(s[start])
                     start += 1
-                print(uniqueChar, i, start)
-            ans += 1
-            print("Ans:", ans)
-            uniqueChar.remove(s[i])
+                # print(uniqueChar, i, start)
+            if len(uniqueChar) == k:
+                ans += 1
+                # print("Ans:", ans)
+                uniqueChar.remove(s[i])
+                i += 1
 
         print(ans)
 
 
 ans = SlidingWindow
-ans.operation("geeksforgeeks", 4)
+ans.operation("abcbac", 3)
