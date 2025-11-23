ab89436db6f8f2f1111f2235e0070ddcc1e3575d
DevarshiKothari <devarshikothari1998@gmail.com>
2025-11-23 18:25:54 +0530

    added dict in place of set

--- SlidingWindow5.py
+++ SlidingWindow5.py
@@ -1,22 +1,25 @@
 # Longest Substring With At Most K Distinct Characters
+from collections import defaultdict
 
 
 class SlidingWindow:
     def operation(s: str, k: int) -> int:
-        unique = set()
+        unique = defaultdict(int)
         left, right = 0, 0
         ans = 0
         sub: str = ""
 
         while right < len(s):
-            unique.add(s[right])
+            unique[s[right]] += 1  # expansion
             if len(unique) <= k:
                 if right - left + 1 > ans:
                     ans = right - left + 1
                     sub = s[left : right + 1]
             else:
-                while len(unique) > k:
-                    unique.remove(s[left])
+                while len(unique) > k:  # shrink
+                    unique[s[left]] -= 1
+                    if unique[s[left]] == 0:
+                        del unique[s[left]]
                     left += 1
             right += 1
         print(sub)
