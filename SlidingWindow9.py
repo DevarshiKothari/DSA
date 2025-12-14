0ed4435df61bbdaf6ee9bc2dfe2922809d1c9393
DevarshiKothari <devarshikothari1998@gmail.com>
2025-12-14 22:01:15 +0530

    optimized solution

--- SlidingWindow9.py
+++ SlidingWindow9.py
@@ -5,29 +5,30 @@ class Solution:
     def characterReplacement(self, s, k):
         s = s.lower()
         left = 0
-        max = 0
-        d = k
+        ans = 0
         freq = [0] * 26
         for right in range(len(s)):
-            freq[ord(s[right]) - 97] += 1
-            if s[left] != s[right]:
-                d -= 1
-            while d < 0 or (right - left + 1 - freq[left] > k and left != right):
-                while s[left] == s[left + 1]:
-                    freq[left] -= 1
-                    left += 1
-                freq[left] -= 1
+            freq[ord(s[right]) - 97] += 1 # expansion
+            max_freq = max(freq)
+            total_sum = sum(freq)
+            d = k - (total_sum - max_freq)
+
+            while d < 0 and left < right: # contraction
+                freq[ord(s[left]) - 97] -= 1
                 left += 1
-                d = right - left + 1 - freq[left]
-            if right - left + 1 > max:
-                max = right - left + 1
-        return max
+                max_freq = max(freq)
+                total_sum = sum(freq)
+                d = k - (total_sum - max_freq)
+
+            if right - left + 1 > ans: # condition
+                ans = right - left + 1
+        return ans
 
 
 sol = Solution()
-print(sol.characterReplacement("ABAB", 2))
-print(sol.characterReplacement("AABABBA", 1))
-print(sol.characterReplacement("AAAA", 0))
-print(sol.characterReplacement("AAAB", 0))
-print(sol.characterReplacement("ABAA", 0))
-print(sol.characterReplacement("ABCBDBFFFA", 3))
+print(
+    sol.characterReplacement(
+        "EOEMQLLQTRQDDCOERARHGAAARRBKCCMFTDAQOLOKARBIJBISTGNKBQGKKTALSQNFSABASNOPBMMGDIOETPTDICRBOMBAAHINTFLH",
+        7,
+    )
+)
